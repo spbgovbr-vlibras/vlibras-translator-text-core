dbe6a747f5aab87336bafe5933f9ac005c8c2b28
Wesnydy Ribeiro <wesnydy@lavid.ufpb.br>
2019-03-25 13:43:09 -0300

    Removal of configreader module class

--- core/utils/configreader.py
+++ core/utils/configreader.py
@@ -4,16 +4,13 @@ import os
 _CONFIG_FILE = os.environ.get("CORE_CONFIG_FILE", "")
 _CONFIG = configparser.ConfigParser()
 
-class ConfigReader:
-    
-    @staticmethod
-    def load_configs(section):
-        try:
-            with open(_CONFIG_FILE) as c_file:
-                _CONFIG.read_file(c_file)
+def load_configs(section):
+    try:
+        with open(_CONFIG_FILE) as c_file:
+            _CONFIG.read_file(c_file)
 
-        except FileNotFoundError:
-            print("Configuration file not found")
-            return None
+    except FileNotFoundError:
+        print("Configuration file not found")
+        return None
 
-        return _CONFIG[section] if section in _CONFIG else None
+    return _CONFIG[section] if section in _CONFIG else None
