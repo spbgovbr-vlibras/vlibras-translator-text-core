a697ec5465f43ab0377b01210580a69e99531fdf
Wesnydy Ribeiro <wesnydy@lavid.ufpb.br>
2019-06-28 14:09:41 -0300

    Removing translator-lib files from core

--- worker/vlibras-translate/src/Espanhol/LerDicionarios_Espanhol.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-from TemplateLerDicionarios import TemplateLerDicionarios
-
-class LerDicionarios(TemplateLerDicionarios):
-	"""docstring for LerDicionarios"""
-	def __init__(self):
-		TemplateLerDicionarios.__init__(self)
