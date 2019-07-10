25d72d893a90569b8669636f99b01c7b8faa06d4
Wesnydy Ribeiro <wesnydy@lavid.ufpb.br>
2019-07-10 19:03:31 -0300

    Adding avatar selection parameter and returning size and duration of the libras video

--- worker/core/player/playerwrapper.py
+++ worker/core/player/playerwrapper.py
@@ -4,7 +4,7 @@ import os
 import shutil
 import subprocess
 
-# import pyvirtualdisplay
+import pyvirtualdisplay
 
 from utils import configreader
 
@@ -16,12 +16,24 @@ class PlayerWrapper:
         self.__ffmpegcfg = configreader.load_configs("FFmpeg")
         self.__temp_dir = self.__playercfg.get("TempDir", "/tmp")
 
-    def __start_frame_capture(self, input_file, correlation_tag):
+    def __start_frames_capture(
+        self,
+        input_file: str,
+        correlation_tag: str,
+        player_params: dict
+    ) -> str:
+
         frames_dir = os.path.join(self.__temp_dir, correlation_tag, "frames")
         frames_path = os.path.join(frames_dir, "img_%08d.png")
 
         display_width = self.__playercfg.get("DisplayWidth", "640")
         display_height = self.__playercfg.get("DisplayHeight", "480")
+
+        avatar_param = (player_params.get("avatar")
+                        or self.__playercfg.get("Avatar", "icaro"))
+
+        animation_speed = (player_params.get("speed")
+                           or self.__playercfg.get("AnimationSpeed", "60"))
 
         unity_cmd = [
             self.__playercfg.get("PlayerBin", "None"),
@@ -30,14 +42,15 @@ class PlayerWrapper:
             frames_dir,
             display_width,
             display_height,
-            self.__playercfg.get("AnimationSpeed", "60"),
-            self.__playercfg.get("VideoFramerate", "24")]
+            animation_speed,
+            self.__playercfg.get("VideoFramerate", "24"),
+            avatar_param]
 
-        # xephyr = pyvirtualdisplay.Display(
-        #     visible=False,
-        #     size=(display_width, display_height))
+        xephyr = pyvirtualdisplay.Display(
+            visible=False,
+            size=(display_width, display_height))
 
-        # xephyr.start()
+        xephyr.start()
 
         try:
             if not os.path.exists(frames_dir):
@@ -57,10 +70,15 @@ class PlayerWrapper:
             frames_path = ""
 
         finally:
-            # xephyr.stop()
+            xephyr.stop()
             return frames_path
 
-    def __start_video_rendering(self, frames_path, correlation_tag):
+    def __start_video_rendering(
+        self,
+        frames_path: str, 
+        correlation_tag: str
+    ) -> str:
+
         video_name = "{}.{}".format(correlation_tag, "mp4")
         video_path = os.path.join(self.__temp_dir, correlation_tag, video_name)
 
@@ -93,14 +111,21 @@ class PlayerWrapper:
             shutil.rmtree(os.path.dirname(frames_path), ignore_errors=True)
             return video_path
 
-    def run(self, input_text, correlation_tag):
+    def run(
+        self, 
+        input_text: str,
+        correlation_tag: str,
+        options: dict = {}
+    ) -> tuple:
+
         gloss_name = "{}.{}".format(correlation_tag, "txt")
         gloss_path = os.path.join(self.__temp_dir, correlation_tag, gloss_name)
 
+        os.makedirs(os.path.dirname(gloss_path), exist_ok=True)
         with open(gloss_path, "w") as gfile:
             gfile.write("{}#0".format(input_text))
 
-        frames = self.__start_frame_capture(gloss_path, correlation_tag)
+        frames = self.__start_frames_capture(gloss_path, correlation_tag, options)
         if not frames:
             return None
 
@@ -108,4 +133,13 @@ class PlayerWrapper:
         if not libras_video:
             return None
 
-        return  libras_video
+        libras_video_size = 0
+        try:
+            libras_video_size = os.path.getsize(libras_video)
+        except:
+            self.__logger.warning("Libras video size was not calculated.")
+            pass
+
+        libras_video_duration = 0
+
+        return  libras_video, libras_video_size, libras_video_duration
