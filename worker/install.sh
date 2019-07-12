4e8e22dfded3b4f158f8cdf3fd52a4e5f013ffac
Wesnydy Ribeiro <wesnydy@lavid.ufpb.br>
2019-07-12 14:10:20 -0300

    Seting python interpreter to python3.6

--- worker/install.sh
+++ worker/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-LINUX_PACKAGES="build-essential python3-pip ffmpeg xvfb xserver-xephyr vnc4server"
+LINUX_PACKAGES="build-essential python3.6-pip ffmpeg xvfb xserver-xephyr vnc4server"
 
 function install_system_dependencies {
   echo "Installing required system dependencies..."
@@ -10,7 +10,7 @@ function install_system_dependencies {
 
 function install_python_packages {
   echo -e "\nInstalling required python packages..."
-  (python3 -m pip install -r requirements.txt) || return 1
+  (python3.6 -m pip install -r requirements.txt) || return 1
   return 0
 }
 
