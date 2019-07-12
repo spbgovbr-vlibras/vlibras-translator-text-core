3bb04c3cc8ae833e98ba00f947feae8623b9aeab
Wesnydy Ribeiro <wesnydy@lavid.ufpb.br>
2019-07-12 14:13:05 -0300

    Setting Python interpreter to 3.6

--- worker/install.sh
+++ worker/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-LINUX_PACKAGES="build-essential python3-pip libhunspell-dev openjdk-8-jdk"
+LINUX_PACKAGES="build-essential python3.6-pip libhunspell-dev openjdk-8-jdk"
 
 function install_system_dependencies {
   echo "Installing required system dependencies..."
@@ -10,7 +10,7 @@ function install_system_dependencies {
 
 function install_python_packages {
   echo -e "\nInstalling required python packages..."
-  (python3 -m pip install -r requirements.txt) || return 1
+  (python3.6 -m pip install -r requirements.txt) || return 1
   return 0
 }
 
