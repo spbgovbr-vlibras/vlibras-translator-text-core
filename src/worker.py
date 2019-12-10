35b0ff09af21fb3995df14a5d22a4811cbf8ed78
Wesnydy Ribeiro <wesnydy@gmail.com>
2019-12-10 20:42:15 -0300

    Adding dl translator to worker

--- src/worker.py
+++ src/worker.py
@@ -3,68 +3,26 @@
 import json
 import logging.config
 import os
-import threading
 
 from vlibras_translate import translation
 
-from utils import configreader
-from utils import queuewrapper
+from util import configreader
+from util import exceptionhandler
+from util import queuewrapper
 
 
 class Worker:
 
-    def __init__(self):
-        self.__logger = logging.getLogger(__class__.__name__)
-        self.__translator = self.__get_translator()
+    def __init__(self, dlmode):
+        self.__logger = logging.getLogger(self.__class__.__name__)
+
+        if (dlmode == "true"):
+            self.__translate = translation.Translation().rule_translation_with_dl
+        else:
+            self.__translate = translation.Translation().rule_translation
+
         self.__consumer = queuewrapper.QueueConsumer()
         self.__publisher = queuewrapper.QueuePublisher()
-
-    def __get_translator(self):
-        try:
-            self.__logger.debug("Instantiating vlibras_translate.")
-            return translation.Translation()
-        except vlibras_translate.exceptions.LemmaException as ex:
-            raise ex
-
-    def __process_message(self, channel, method, properties, body):
-        try:
-            self.__logger.info("Processing a new translation request.")
-            payload = json.loads(body)
-            gloss = self.__translator.rule_translation(payload.get("text", ""))
-
-            self.__reply_message(
-                route=properties.reply_to,
-                message=json.dumps({"translation": gloss}),
-                id=properties.correlation_id)
-
-        except json.JSONDecodeError:
-            self.__logger.exception("Received an invalid translation request.")
-            self.__reply_message(
-                route=properties.reply_to,
-                message=json.dumps({"error": "Body is not a valid JSON"}),
-                id=properties.correlation_id)
-
-        except vlibras_translate.exceptions.LemmaException:
-            self.__logger.exception(
-                "vlibras_translate failed to translate text.")
-            self.__reply_message(
-                route=properties.reply_to,
-                message=json.dumps({"error": "Failed to translate text."}),
-                id=properties.correlation_id)
-
-            del self.__translator
-            self.__translator = self.__get_translator()
-
-        except Exception:
-            self.__logger.exception("An unexpected exception occurred.")
-            self.__reply_message(
-                route=properties.reply_to,
-                message=json.dumps({"error": "Translator internal error."}),
-                id=properties.correlation_id)
-
-        finally:
-            if channel.is_open:
-                channel.basic_ack(delivery_tag=method.delivery_tag)
 
     def __reply_message(self, route, message, id):
         self.__logger.info("Sending response to request.")
@@ -76,6 +34,29 @@ class Worker:
             self.__logger.error("The request don't have reply_to route.")
         else:
             self.__publisher.publish_to_queue(route, message, id)
+
+    def __process_message(self, channel, method, properties, body):
+        try:
+            self.__logger.info("Processing a new translation request.")
+            payload = json.loads(body)
+            gloss = self.__translate(payload.get("text", ""))
+
+            self.__reply_message(
+                route=properties.reply_to,
+                message=json.dumps({"translation": gloss}),
+                id=properties.correlation_id)
+
+        except Exception as ex:
+            exceptionhandler.handle_exception(ex)
+
+            self.__reply_message(
+                route=properties.reply_to,
+                message=json.dumps({"error": "Translator internal error."}),
+                id=properties.correlation_id)
+
+        finally:
+            if channel.is_open:
+                channel.basic_ack(delivery_tag=method.delivery_tag)
 
     def start(self, queue):
         self.__logger.debug("Starting queue consumer.")
@@ -99,7 +80,7 @@ if __name__ == "__main__":
 
     try:
         logger.info("Creating Translation Worker.")
-        worker = Worker()
+        worker = Worker(workercfg.get("DLTranslationMode", "false"))
         logger.info("Starting Translation Worker.")
         worker.start(workercfg.get("TranslatorQueue"))
 
