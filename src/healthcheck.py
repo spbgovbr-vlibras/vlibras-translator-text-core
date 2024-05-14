da4396b4b72ebcda78a687ded0e1a604d335f1d2
diego.silva <diego.silva@lavid.ufpb.br>
2024-05-14 18:30:25 -0300

    rework: move worker processing to a secondary thread

--- src/healthcheck.py
+++ src/healthcheck.py
@@ -2,11 +2,13 @@ import http.server
 import logging
 import threading
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
 
 class HealthcheckHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
 
     def __init__(self, *args, **kwargs):
-        self.__logger = logging.getLogger(self.__class__.__name__)
         super().__init__(*args, **kwargs)
 
     def do_GET(self):
@@ -16,12 +18,11 @@ class HealthcheckHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         self.wfile.write("OK".encode())
 
     def log_message(self, format, *args):
-        self.__logger.debug("Healthcheck from %s %s" %
-                            (self.address_string(), format % args))
+        logger.debug("Healthcheck from %s %s" %
+                     (self.address_string(), format % args))
 
 
 def _create_healthcheck_Server(port):
-    logger = logging.getLogger(__name__)
     logger.debug("Starting healthcheck server on port {}.".format(port))
     server = http.server.HTTPServer(('', port), HealthcheckHTTPRequestHandler)
     server.serve_forever()
